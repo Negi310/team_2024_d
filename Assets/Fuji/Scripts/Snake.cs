ebec88848735d6c5a6b85cad9dbffdee85b34a6e
Negi310 <f.satopec@icloud.com>
2024-09-14 04:59:23 +0900

    ヘビ型ボスのプロトタイプ
    ヘビの頭が自走するようにした。
    プロトタイプとしてADキーでヘビの頭の進行方向を変更できるようにした。
    ヘビの身体をプレハブで生成し、ヘビの頭の動きを遅れて追従するようにした。

--- Assets/Fuji/Scripts/Snake.cs
+++ Assets/Fuji/Scripts/Snake.cs
@@ -2,17 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Snake : MonoBehaviour
-{
+public class Snake : Enemy
+{   
+    public float moveSpeed = 5f; 
+
+    public float steerSpeed = 180f;
+
+    public float bodySpeed = 5f;
+    
+    public int gap = 10;
+
+    public GameObject snakeBody;
+
+    public GameObject snakeBody2;
+
+    public List<GameObject> bodyParts = new List<GameObject>();
+
+    public List<Vector3> bodyLogs = new List<Vector3>();
+
     // Start is called before the first frame update
     void Start()
     {
+        for(int i = 0 ; i < 4 ; i++)
+        {
+            GrowSnake2();
+            GrowSnake();
+        }
+    }
+
+    void Update()
+    {
+        float steerDirection = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.left * steerDirection * steerSpeed * Time.deltaTime);
+
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        bodyLogs.Insert(0, transform.position);
+        int index = 0;
+        foreach (var body in bodyParts)
+        {
+            Vector3 point = bodyLogs[Mathf.Min(index * gap, bodyLogs.Count-1)];
+            Vector3 moveDirection = point - body.transform.position;
+            body.transform.position += moveDirection * bodySpeed * Time.deltaTime;
+            body.transform.LookAt(point);
+            index++;
+        }
+        if (bodyLogs.Count > bodyParts.Count * gap)
+        {
+            bodyLogs.RemoveAt(bodyLogs.Count - 1);
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GrowSnake()
     {
-        
-    }
+        GameObject body = Instantiate(snakeBody);
+        bodyParts.Add(body);
+    } 
+    private void GrowSnake2()
+    {
+        GameObject body = Instantiate(snakeBody2);
+        bodyParts.Add(body);
+    } 
 }
