a259daea47920e2b581fd9558ce8eecc7ddc98b9
YamamotoTaisei-Pr <s2410293.yamamoto.cgp@gmail.com>
2024-09-29 21:15:50 +0900

    リザルト画面
    リザルト画面への移行と再スタートのスクリプトの反映

--- Assets/Fuji/Scripts/GameManager.cs
+++ Assets/Fuji/Scripts/GameManager.cs
@@ -29,7 +29,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerRoot == null || !playerRoot.activeInHierarchy)
+        if (playerRoot == null)
         {
             GameOver();
         }
